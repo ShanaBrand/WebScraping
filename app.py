f124d33335179df87ea153ab6944faec1d2f0184
ShanaBrand <shanakay.brandford@gmail.com>
2018-08-24 20:41:38 -0700

    WebScrapingMongo

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import scrape_mars
 
 app = Flask(__name__)
 
+app.config["MONGO_URI"] = "mongodb://localhost:27017/weather_app"
+mongo = PyMongo(app)
+
+
 #conn = 'mongodb://localhost:27017'
 
 # setup mongo connection
@@ -11,19 +15,50 @@ conn = "mongodb://localhost:27017"
 client = pymongo.MongoClient(conn)
 
 # connect to mongo db and collection
-db = client.store_inventory
-collection = db.produce
+db = client.marsDb
+db.scrapeMars.drop()
 
 
 @app.route("/")
 def index():
     # write a statement that finds all the items in the db and sets it to a variable
-    inventory = list(db.collection.find())
-    print(inventory)
 
+   scrapeMars = mongo.db.collection.find()
     # render an index.html template and pass it the data you retrieved from the database
-    return render_template("index.html", inventory=inventory)
+   return render_template("marsIndex.html", scrapeMars=scrapeMars)
+
+
+@app.route("/scrape")
+
+def scrape_info():
+
+    # Run scraped functions
+    myScrape = scrape_mars.scrape()
+
+    # Store results into a dictionary
+    marsdata = {
+        "news": myScrape["latest_News"],
+        "featuredImage": myScrape["feaured_Img"],
+        "marsWeather": myScrape["latest_weather"],
+        "marsFacts": myScrape["marsFacts"],
+        "marHemisphere": myScrape["mars_hemisphere"],
+    }
+
+    # Insert forecast into database
+    mongo.db.collection.insert_one(marsdata)
+
+    # Redirect back to home page
+    return redirect("/", code=302)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+  <div class="jumbotron text-center">
+      <!-- Render our data -->
+      <h3>Mars Hemisphere</h3> 
+      
+       </div>
--- scrape_mars.py
+++ scrape_mars.py
@@ -4,86 +4,99 @@ from splinter import Browser
 import pymongo
 import pandas as pd
 
+
 executable_path = {'executable_path': 'chromedriver.exe'}
 browser = Browser('chrome', **executable_path, headless=False)
 
 
-conn = 'mongodb://localhost:27017'
-client = pymongo.MongoClient(conn);
-
-def scrape():
-
-    nasa_url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
-    jpl_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-    mw_url = 'https://twitter.com/marswxreport?lang=en'
-    mf_url = 'https://space-facts.com/mars/'
-    mh_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-
-    nasa_resp = requests.get(nasa_url)
-    jpl_resp = requests.get(jpl_url)
-    mw_resp = requests.get(mw_url)
-    mf_resp = requests.get(mf_url)
-    mh_resp = requests.get(mh_url)
+#conn = 'mongodb://localhost:27017'
+#client = pymongo.MongoClient(conn);
+#db = client.marsDB
+#marsData = db.marsData.find()
 
 ##NASA MARS NEWS
-        #Scrape the NASA Mars News Site and collect the latest News Title and Paragraph Text.
-        #Assign the text to variables that you can reference later.
-    n_soup = bs(nasa_resp.text,'html.parser')
-    print(n_soup.prettify())
+#Scrape the NASA Mars News Site and collect the latest News Title and Paragraph Text.
+# #Assign the text to variables that you can reference later.
+
+def latest_News():
+    nasa_url = 'https://mars.nasa.gov/news/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest'
+    nasa_resp = requests.get(nasa_url) 
     
-    news_title = n_soup.find_all('div',class_='content_title')
-    for n in news_title:
-        print("TITLE ", n.text)
-        title = n.text
-        
-    news_para = n_soup.find_all('div',class_='rollover_description_inner')       
-    for p in news_para:
-        paragraph = p.text
-        print("PARAGRAPH", p.text) 
+    n_soup = bs(nasa_resp.text,'html.parser')
+    
+    title= n_soup.find_all('div',class_='content_title')
+    news_title = title[0].text
+
+    
+    para = n_soup.find_all('div',class_='article_teaser_body')   
+    news_para = para[0].text
+    
+    return news_title, news_para
 
 ##JPL Featured Image
-        ##Use splinter to navigate the site and find the image url for the current Featured Mars Image and 
-        #assign the url string to a variable called featured_image_url.
-        #Make sure to find the image url to the full size .jpg image.
-        #Make sure to save a complete url string for this image.
+##Use splinter to navigate the site and find the image url for the current Featured Mars Image and 
+#assign the url string to a variable called featured_image_url.
+#Make sure to find the image url to the full size .jpg image.
+#Make sure to save a complete url string for this image.
+
+def featured_Img():
+    jpl_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    
     browser.visit(jpl_url)
     jpl_html = browser.html
     j_soup = bs(jpl_html,'lxml')
     
     ft_img = j_soup.find('article', class_='carousel_item')['style'][23:75]
     ft_img_url = jpl_url+ft_img
-    ft_img_url 
+    ft_img_url
+    
+    return ft_img_url
 
-##MARS Weather Twitter
-        #Visit the Mars Weather twitter account here and scrape the latest Mars weather tweet from the page. 
-        #Save the tweet text for the weather report as a variable called mars_weather.
-    mw_soup= bs(mw_resp.text,'lxml')
-    print(mw_soup)
+
+ ##MARS Weather Twitter
+ #Visit the Mars Weather twitter account here and scrape the latest Mars weather tweet from the page. 
+ #Save the tweet text for the weather report as a variable called mars_weather.
+def mars_Weather():
+    mw_url = 'https://twitter.com/marswxreport?lang=en'
+    mw_resp = requests.get(mw_url)
+    mw_soup = bs(mw_resp.text,'lxml')
+    #print(mw_soup)
     tweet = mw_soup.find_all('div',class_="js-tweet-text-container")
     mars_weather = tweet[0].text
+    
+    return mars_weather()
+
+
 
 
 
 ##MARS Facts Table
-    #Visit the Mars Facts webpage here and use Pandas to scrape the table containing facts about the planet including
-    #Diameter, Mass, etc.
-    #Use Pandas to convert the data to a HTML table string.
+#Visit the Mars Facts webpage here and use Pandas to scrape the table containing facts about the planet including
+#Diameter, Mass, etc.
+#Use Pandas to convert the data to a HTML table string.
+
+def mars_Facts():
+    mf_url = 'https://space-facts.com/mars/'
 
     fact_tbl = pd.read_html(mf_url)
     fact_tbl
 
     df_tbl = fact_tbl[0]
     df_tbl
-
+    return df_tbl
 
 #MARS HEMISPHERE
-        # Visit the USGS Astrogeology site here to obtain high resolution images for each of Mar's hemispheres.
-        # You will need to click each of the links to the hemispheres in order to find the image url to the full resolution image.
-        # Save both the image url string for the full resolution hemisphere image, and the Hemisphere title containing
-        # the hemisphere name. Use a Python dictionary to store the data using the keys img_url and title.
-        # Append the dictionary with the image url string and the hemisphere title to a list.
-        # This list will contain one dictionary for each hemisphere.
-    
+#Visit the USGS Astrogeology site here to obtain high resolution images for each of Mar's hemispheres.
+# You will need to click each of the links to the hemispheres in order to find the image url to the full resolution image.
+# Save both the image url string for the full resolution hemisphere image, and the Hemisphere title containing
+# the hemisphere name. Use a Python dictionary to store the data using the keys img_url and title.
+# Append the dictionary with the image url string and the hemisphere title to a list.
+# This list will contain one dictionary for each hemisphere.
+
+def mars_Hemisphere():
+    mh_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    mh_resp = requests.get(mh_url)
+
     mh_soup = bs(mh_resp.text,'lxml')
         
     hem_dic = {}
@@ -116,11 +129,20 @@ def scrape():
 
         hem_dic.update({'img_url':(img_url)})
 
-            
+          
     myList.append(hem_dic)
+    
+    return myList
 
-
-    print(myList)
+   
+def scrape():
+    data_dict = {
+        'latest_News':latest_News(),
+        'featured_Img': featured_Img(),
+        'latest_weather':mars_Weather(),
+        'marsFacts':mars_Facts(),
+        'mars_hemisphere':mars_Hemisphere()}
+    return data_dict
 
         
     
